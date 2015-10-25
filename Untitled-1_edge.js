efb2ab2dbd2e8b818b3acf1ec34c14840a1d601a
myxvisual <myxvisual@live.com>
2015-10-26 00:08:57 +0800

    test
    hosts

---
+++ Untitled-1_edge.js
@@ -0,0 +1,79 @@
+/*jslint */
+/*global AdobeEdge: false, window: false, document: false, console:false, alert: false */
+(function (compId) {
+
+    "use strict";
+    var im='images/',
+        aud='media/',
+        vid='media/',
+        js='js/',
+        fonts = {
+        },
+        opts = {
+            'gAudioPreloadPreference': 'auto',
+            'gVideoPreloadPreference': 'auto'
+        },
+        resources = [
+        ],
+        scripts = [
+        ],
+        symbols = {
+            "stage": {
+                version: "6.0.0",
+                minimumCompatibleVersion: "5.0.0",
+                build: "6.0.0.400",
+                scaleToFit: "none",
+                centerStage: "none",
+                resizeInstances: false,
+                content: {
+                    dom: [
+                        {
+                            id: 'Text',
+                            type: 'text',
+                            rect: ['102px', '178px', 'auto', 'auto', 'auto', 'auto'],
+                            text: "<p style=\"margin: 0px;\">​asdsadsad</p>",
+                            font: ['Arial, Helvetica, sans-serif', [24, ""], "rgba(0,0,0,1)", "normal", "none", "", "break-word", "nowrap"]
+                        }
+                    ],
+                    style: {
+                        '${Stage}': {
+                            isStage: true,
+                            rect: ['null', 'null', '550px', '400px', 'auto', 'auto'],
+                            overflow: 'hidden',
+                            fill: ["rgba(255,255,255,0.00)"]
+                        }
+                    }
+                },
+                timeline: {
+                    duration: 415,
+                    autoPlay: true,
+                    data: [
+                        [
+                            "eid3",
+                            "left",
+                            95,
+                            320,
+                            "linear",
+                            "${Text}",
+                            '102px',
+                            '152px'
+                        ],
+                        [
+                            "eid4",
+                            "top",
+                            95,
+                            320,
+                            "linear",
+                            "${Text}",
+                            '178px',
+                            '219px'
+                        ]
+                    ]
+                }
+            }
+        };
+
+    AdobeEdge.registerCompositionDefn(compId, symbols, fonts, scripts, resources, opts);
+
+    if (!window.edge_authoring_mode) AdobeEdge.getComposition(compId).load("Untitled-1_edgeActions.js");
+})("EDGE-240719732");

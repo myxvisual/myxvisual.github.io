57a1155144a44b6ddbf0cfaa90cc024117a442ae
myxvisual <myxvisual@live.com>
2016-03-16 18:04:34 +0800

    add style

--- js/mark-X.js
+++ js/mark-X.js
@@ -1,5 +1,5 @@
 var md = window.markdownit();
-var $ = function (element) {
+function $(element) {
   var selector = element.match(/([.#])*(.+)/);
   switch (selector[1]) {
     case '.':
@@ -11,7 +11,7 @@ var $ = function (element) {
     default:
       return document.getElementsByTagName(selector[2]);
   }
-};
+}
 
 function renderMD(data) {
   function replacer(match, p1, offset, string) {
@@ -29,4 +29,158 @@ function renderMD(data) {
       maths[i].innerHTML += '\n\n' + children;
     })
   }
+}
+
+function syncScroll(element) {
+  var scrollSynctor = element.scrollTop / element.scrollHeight;
+  $('#preview').scrollTop = $('#preview').scrollHeight * scrollSynctor;
+}
+
+function createString(type) {
+  switch (type) {
+    case type = 'bold':
+      window.setTimeout(function () {
+        $('#mdbody').focus();
+      }, 0);
+      var startPos = $('#mdbody').selectionStart;
+      var endPos = $('#mdbody').selectionEnd;
+      $('#mdbody').value = $('#mdbody').value.substring(0, startPos)
+        + '****'
+        + $('#mdbody').value.substring(endPos, $('#mdbody').value.length);
+      $('#mdbody').selectionStart = startPos + 2;
+      $('#mdbody').selectionEnd = endPos + 2;
+      break;
+
+    case type = 'italic':
+      window.setTimeout(function () {
+        $('#mdbody').focus();
+      }, 0);
+      var startPos = $('#mdbody').selectionStart;
+      var endPos = $('#mdbody').selectionEnd;
+      $('#mdbody').value = $('#mdbody').value.substring(0, startPos)
+        + '**'
+        + $('#mdbody').value.substring(endPos, $('#mdbody').value.length);
+      $('#mdbody').selectionStart = startPos + 1;
+      $('#mdbody').selectionEnd = endPos + 1;
+      break;
+
+    case type = 'strikethrough':
+      window.setTimeout(function () {
+        $('#mdbody').focus();
+      }, 0);
+      var startPos = $('#mdbody').selectionStart;
+      var endPos = $('#mdbody').selectionEnd;
+      $('#mdbody').value = $('#mdbody').value.substring(0, startPos)
+        + '~~~~'
+        + $('#mdbody').value.substring(endPos, $('#mdbody').value.length);
+      $('#mdbody').selectionStart = startPos + 2;
+      $('#mdbody').selectionEnd = endPos + 2;
+      break;
+
+    case type = 'underline':
+      window.setTimeout(function () {
+        $('#mdbody').focus();
+      }, 0);
+      var startPos = $('#mdbody').selectionStart;
+      var endPos = $('#mdbody').selectionEnd;
+      $('#mdbody').value = $('#mdbody').value.substring(0, startPos)
+        + '++++'
+        + $('#mdbody').value.substring(endPos, $('#mdbody').value.length);
+      $('#mdbody').selectionStart = startPos + 2;
+      $('#mdbody').selectionEnd = endPos + 2;
+      break;
+
+    case type = 'h1':
+      window.setTimeout(function () {
+          $('#mdbody').focus();
+      }, 0);
+      var startPos = $('#mdbody').selectionStart;
+      var endPos = $('#mdbody').selectionEnd;
+      $('#mdbody').value = $('#mdbody').value.substring(0, startPos)
+        + '# '
+        + $('#mdbody').value.substring(endPos, $('#mdbody').value.length);
+      $('#mdbody').selectionStart = startPos + 2;
+      $('#mdbody').selectionEnd = endPos + 2;
+      break;
+
+    case type = 'h2':
+      window.setTimeout(function () {
+        $('#mdbody').focus();
+      }, 0);
+      var startPos = $('#mdbody').selectionStart;
+      var endPos = $('#mdbody').selectionEnd;
+      $('#mdbody').value = $('#mdbody').value.substring(0, startPos)
+        + '## '
+        + $('#mdbody').value.substring(endPos, $('#mdbody').value.length);
+      $('#mdbody').selectionStart = startPos + 3;
+      $('#mdbody').selectionEnd = endPos + 3;
+      break;
+
+    case type = 'h3':
+      window.setTimeout(function () {
+        $('#mdbody').focus();
+      }, 0);
+      var startPos = $('#mdbody').selectionStart;
+      var endPos = $('#mdbody').selectionEnd;
+      $('#mdbody').value = $('#mdbody').value.substring(0, startPos)
+        + '### '
+        + $('#mdbody').value.substring(endPos, $('#mdbody').value.length);
+      $('#mdbody').selectionStart = startPos + 4;
+      $('#mdbody').selectionEnd = endPos + 4;
+      break;
+
+    case type = 'h4':
+      window.setTimeout(function () {
+          $('#mdbody').focus();
+      }, 0);
+      var startPos = $('#mdbody').selectionStart;
+      var endPos = $('#mdbody').selectionEnd;
+      $('#mdbody').value = $('#mdbody').value.substring(0, startPos)
+        + '#### '
+        + $('#mdbody').value.substring(endPos, $('#mdbody').value.length);
+      $('#mdbody').selectionStart = startPos + 5;
+      $('#mdbody').selectionEnd = endPos + 5;
+      break;
+
+    case type = 'h5':
+      window.setTimeout(function () {
+        $('#mdbody').focus();
+      }, 0);
+      var startPos = $('#mdbody').selectionStart;
+      var endPos = $('#mdbody').selectionEnd;
+      $('#mdbody').value = $('#mdbody').value.substring(0, startPos)
+        + '##### '
+        + $('#mdbody').value.substring(endPos, $('#mdbody').value.length);
+      $('#mdbody').selectionStart = startPos + 6;
+      $('#mdbody').selectionEnd = endPos + 6;
+      break;
+
+    case type = 'h6':
+      window.setTimeout(function () {
+        $('#mdbody').focus();
+      }, 0);
+      var startPos = $('#mdbody').selectionStart;
+      var endPos = $('#mdbody').selectionEnd;
+      $('#mdbody').value = $('#mdbody').value.substring(0, startPos)
+        + '###### '
+        + $('#mdbody').value.substring(endPos, $('#mdbody').value.length);
+      $('#mdbody').selectionStart = startPos + 7;
+      $('#mdbody').selectionEnd = endPos + 7;
+      break;
+
+
+
+
+    default:
+
+  }
+}
+
+
+
+function iconDown(element) {
+  element.style.cssText += ';color:rgba(158, 238, 255, 0.7)';
+}
+function iconUp(element) {
+  element.style.cssText -= ';color:rgba(158, 238, 255, 0.7)';
 }
